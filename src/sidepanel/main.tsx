ced3a46cac5f75b118499a057476fb881c2676ef
Ben Liu <ben196888@gmail.com>
2024-11-17 01:08:13 +0700

    fix: filename should not start with _

--- src/sidepanel/main.tsx
+++ src/sidepanel/main.tsx
@@ -1,4 +1,4 @@
 import App from '../components/App.tsx'
-import { renderComponent } from '../_page.tsx'
+import { renderComponent } from '../page.tsx'
 
 renderComponent('root', <App pageName='Side panel page' />)
